cb7f261210797cbeb28d1d388b0de7c6b518df59
nomhoi <n_vova@mail.ru>
2020-02-11 19:55:08 +0800

    Ordering added to TimestampedMixin.

--- core/models.py
+++ core/models.py
@@ -6,6 +6,9 @@ class TimestampedMixin:
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_at', '-updated_at']
+
 
 class AbstractBaseModel(Model):
     id = fields.IntField(pk=True)
